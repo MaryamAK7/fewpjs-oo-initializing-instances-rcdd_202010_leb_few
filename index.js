17934441765674dcfe7fbba4d9c2cb3e5ed73fef
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-13 19:06:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ class Lunch{
 }
 
 class Dinner{
-  constructor(food,drink){
-    this.food = food;
-    this.drink = drink;
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
   }
 }
